8dad07d0dc387b50d497fb6ee8a1e4af563fc509
Sysdrumatic <sysdrummatic@gmail.com>
2024-12-24 01:35:40 +0100

    Fixing the bug in the app.js file

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const swaggerOptions = require("./api-specs/api-v1.json");
 
 const app = express();
+const port = 3000;
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 const helloEndpoint = require("./components/hello");
 const goodbyeEndpoint = require("./components/goodbye");
@@ -12,9 +16,15 @@ const goodbyeEndpoint = require("./components/goodbye");
 app.use("/api/v1/hello", helloEndpoint);
 app.use("/api/v1/goodbye", goodbyeEndpoint);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || port;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
